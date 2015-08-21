a3e93216c5c1d6c259e5f4343130172f53d0de12
Ashwin <ashyv97@gmail.com>
2015-08-20 23:04:42 -0700

    Simulate long running connection

--- hello.js
+++ hello.js
@@ -2,6 +2,11 @@ var http = require('http'); // How we require modules
 
 http.createServer(function(request, response){
     response.writeHead(200); // Status code in header
-    response.write("Hello, this is dog"); // Response body
-    response.end(); // Close the connection
+    response.write("Dog is running");
+    
+    setTimeout(function(){
+        response.write("Dog is done"); 
+        response.end(); // Close the connection
+    }, 5000);
+    
 }).listen(8080);
